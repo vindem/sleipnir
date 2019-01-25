cc281e890ddafc5b36afbe244762ca79747212d7
vincenzo <vinc.demaio@gmail.com>
2019-01-25 11:23:58 +0100

    First commit FGCS2019 (Adding workflow package and MontageWorkflow class)

---
+++ src/main/java/at/ac/tuwien/ec/workflow/MontageWorkflow.java
@@ -0,0 +1,31 @@
+package at.ac.tuwien.ec.workflow;
+
+import at.ac.tuwien.ec.model.software.MobileApplication;
+
+public class MontageWorkflow extends MobileApplication {
+
+	@Override
+	public void sampleTasks() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void sampleLinks() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setupTasks() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setupLinks() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
