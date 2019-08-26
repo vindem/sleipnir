1766c570f419a0b6947a1399cefbd38efc432d6f
vincenzo <vinc.demaio@gmail.com>
2019-08-26 10:31:56 +0200

    Adding Z3PoS broker

---
+++ src/main/java/at/ac/tuwien/ec/scheduling/offloading/smt/z3/Z3PoSBroker.java
@@ -0,0 +1,234 @@
+package at.ac.tuwien.ec.scheduling.offloading.smt.z3;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+
+import com.microsoft.z3.ArithExpr;
+import com.microsoft.z3.BoolExpr;
+import com.microsoft.z3.Context;
+import com.microsoft.z3.Expr;
+import com.microsoft.z3.IntExpr;
+import com.microsoft.z3.IntNum;
+import com.microsoft.z3.Model;
+import com.microsoft.z3.RatNum;
+import com.microsoft.z3.RealExpr;
+import com.microsoft.z3.Solver;
+import com.microsoft.z3.Status;
+
+import at.ac.tuwien.ec.model.infrastructure.MobileCloudInfrastructure;
+import at.ac.tuwien.ec.model.infrastructure.computationalnodes.ComputationalNode;
+import at.ac.tuwien.ec.model.infrastructure.computationalnodes.MobileDevice;
+import at.ac.tuwien.ec.model.software.MobileApplication;
+import at.ac.tuwien.ec.model.software.MobileSoftwareComponent;
+import at.ac.tuwien.ec.scheduling.Scheduling;
+import at.ac.tuwien.ec.scheduling.offloading.OffloadScheduler;
+import at.ac.tuwien.ec.scheduling.offloading.OffloadScheduling;
+import scala.Tuple2;
+
+public class Z3PoSBroker extends OffloadScheduler {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -4979418341371131767L;
+	private MobileDevice currentVehicle;
+
+	public Z3PoSBroker(MobileApplication A, MobileCloudInfrastructure I, MobileDevice current) {
+		super();
+		setMobileApplication(A);
+		setInfrastructure(I);
+		this.currentVehicle = current;
+	}
+	
+	public Z3PoSBroker(Tuple2<MobileApplication,MobileCloudInfrastructure> t) {
+		super();
+		loadLibrary();
+		setMobileApplication(t._1());
+		setInfrastructure(t._2());
+	}
+	
+	@Override
+	public ArrayList<? extends Scheduling> findScheduling() {
+		ArrayList<MobileSoftwareComponent> candidateBlock = createCandidateBlock();
+		ArrayList<ComputationalNode> candidateNodes = selectCandidateNodes();
+		
+		Map<String, String> config = new HashMap<String, String>();
+		config.put("model", "true");
+		Context ctx = new Context(config);
+		Solver solver = ctx.mkSolver();
+		
+		ArrayList<OffloadScheduling> scheduling = new ArrayList<OffloadScheduling>();
+		
+		OffloadScheduling currScheduling = new OffloadScheduling();
+		
+		final int blockSize = candidateBlock.size();
+		final int nodeNum = candidateNodes.size();
+		final double[] goals = calculateGoals(this.currentVehicle);
+		
+		IntExpr[][] assVar = new IntExpr[blockSize][nodeNum];
+		
+		for(int i = 0; i < blockSize; i++)
+			for(int j = 0; j < nodeNum; j++)
+				assVar[i][j] = ctx.mkIntConst("x"+i+""+j);
+		
+		BoolExpr[] assignment = new BoolExpr[blockSize];
+		
+		for(int i = 0; i < blockSize; i++)
+		{
+			BoolExpr domain = ctx.mkBool(true);
+			ArithExpr sum = ctx.mkInt(0);
+			for(int j = 0; j < nodeNum; j++) 
+			{
+				domain = ctx.mkAnd(domain,
+						ctx.mkOr(
+								ctx.mkEq(assVar[i][j].simplify(),ctx.mkInt(0)),
+								ctx.mkEq(assVar[i][j].simplify(),ctx.mkInt(1))
+								)
+						);
+				sum = ctx.mkAdd(sum,assVar[i][j]);
+			}
+			assignment[i] = ctx.mkAnd(domain,ctx.mkEq(sum, ctx.mkInt(1)));
+		}
+		
+		RealExpr[][] runtimes = new RealExpr[blockSize][nodeNum];
+		RealExpr[][] costMatrix = new RealExpr[blockSize][nodeNum];
+		RealExpr[][] energyMatrix = new RealExpr[blockSize][nodeNum];
+		
+		//System.out.println("RUNTIME");
+		for(int i = 0; i < blockSize; i++)
+		{
+			for(int j = 0; j < nodeNum; j++)
+			{
+				MobileSoftwareComponent curr = candidateBlock.get(i);
+				
+				runtimes[i][j] = ctx.mkReal(""+curr.getRuntimeOnNode(this.currentVehicle, candidateNodes.get(j), this.currentInfrastructure));
+				costMatrix[i][j] = ctx.mkReal(""+candidateNodes.get(j).computeCost(curr, this.currentInfrastructure));
+				energyMatrix[i][j] = ctx.mkReal(""+computeEnergyFor(this.currentVehicle,candidateNodes.get(j),this.currentInfrastructure));
+			}
+			//System.out.println("");
+		}
+		
+		ArithExpr[] runtimeForTask = new ArithExpr[blockSize];
+		BoolExpr[] rtConstraints = new BoolExpr[blockSize];
+		ArithExpr[] costForTask = new ArithExpr[blockSize];
+		BoolExpr[] costConstraints = new BoolExpr[blockSize];
+		ArithExpr[] energyForTask = new ArithExpr[blockSize];
+		BoolExpr[] energyConstraints = new BoolExpr[blockSize];
+		for(int i = 0; i < blockSize; i++)
+		{
+			ArithExpr rt = ctx.mkMul(runtimes[i][0],assVar[i][0]);
+			runtimeForTask[i] = ctx.mkAdd(rt);
+			ArithExpr cost = ctx.mkMul(costMatrix[i][0],assVar[i][0]);
+			costForTask[i] = ctx.mkAdd(cost);
+			ArithExpr energy = ctx.mkMul(energyMatrix[i][0],assVar[i][0]);
+			energyForTask[i] = ctx.mkAdd(energy);
+			for(int j = 1; j < nodeNum; j++)
+			{
+				rt = ctx.mkMul(runtimes[i][j],assVar[i][j]);
+				runtimeForTask[i] = ctx.mkAdd(runtimeForTask[i],rt);
+				cost = ctx.mkMul(costMatrix[i][j],assVar[i][j]);
+				costForTask[i] = ctx.mkAdd(runtimeForTask[i],cost);
+				energy = ctx.mkMul(energyMatrix[i][j],assVar[i][j]);
+				energyForTask[i] = ctx.mkAdd(energyForTask[i],energy);
+			}
+			
+			rtConstraints[i] = ctx.mkLe(runtimeForTask[i], ctx.mkReal(""+goals[0]));
+			costConstraints[i] = ctx.mkLe(costForTask[i], ctx.mkReal(""+goals[1]));
+			energyConstraints[i] = ctx.mkLe(energyForTask[i], ctx.mkReal(""+goals[2]));
+			//System.out.println(rtConstraints[i]);
+			
+		}
+		
+		BoolExpr taskConstraints[] = new BoolExpr[blockSize];
+		for(int i = 0; i < blockSize; i++)
+			taskConstraints[i] = ctx.mkAnd(rtConstraints[i],costConstraints[i],energyConstraints[i]);
+		
+		solver.add(assignment);
+		solver.add(taskConstraints);
+				
+		Status status = solver.check();
+		
+		
+		Model model = null;
+		if(status == Status.SATISFIABLE)
+		{
+			model = solver.getModel();
+			//System.out.println("Satisfiable!");
+			for(int i = 0; i < blockSize; i++)
+				for(int j = 0; j < nodeNum; j++)
+				{
+					if(model.getConstInterp(assVar[i][j]).equals(ctx.mkInt(1))) 
+					{
+							currScheduling.put(candidateBlock.get(i), candidateNodes.get(j));
+							currScheduling.addRuntime(candidateBlock.get(i), candidateNodes.get(j), this.currentInfrastructure);
+							currScheduling.addCost(candidateBlock.get(i), candidateNodes.get(j), this.currentInfrastructure);
+							currScheduling.addEnergyConsumption(candidateBlock.get(i), candidateNodes.get(j), this.currentInfrastructure);
+					}
+				}
+			scheduling.add(currScheduling);
+			return scheduling;
+		}
+		return null;
+		
+	}
+
+	private double[] calculateGoals(MobileDevice currentVehicle) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	private double computeEnergyFor(MobileDevice currentVehicle, ComputationalNode computationalNode,
+			MobileCloudInfrastructure currentInfrastructure) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	private ArrayList<ComputationalNode> selectCandidateNodes() {
+		ArrayList<ComputationalNode> candidateNodes = new ArrayList<ComputationalNode>();
+		
+		candidateNodes.add(currentVehicle);
+		
+		for(ComputationalNode cn : this.currentInfrastructure.getEdgeNodes().values()) 
+			candidateNodes.add(cn);
+		
+		return candidateNodes;
+	}
+
+	private ArrayList<MobileSoftwareComponent> createCandidateBlock() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	private void loadLibrary()
+	{
+		System.err.println("java.library.path = " + System.getProperty("java.library.path"));
+		System.err.println("trying to load lib z3java");
+		try { 
+			System.load("/home/vincenzo/z3/build/libz3.so");
+			System.load("/home/vincenzo/z3/build/libz3java.so"); 
+		} catch (UnsatisfiedLinkError ex1) { 
+			ex1.printStackTrace();
+			try { 
+				System.err.println("Trying to load lib libz3java");
+				System.loadLibrary("libz3java"); 
+			} catch (UnsatisfiedLinkError ex2) { 
+				ex2.printStackTrace();
+			}
+		}
+	}
+	
+	private static double doubleValue(RealExpr realExpr) {
+		Expr value = realExpr.simplify();
+		if(value.isRatNum())
+		{
+			RatNum rational = (RatNum) value;
+			IntNum num = rational.getNumerator();
+			IntNum den = rational.getDenominator();
+			return ((double) num.getInt64() / den.getInt64());
+		}
+		return ((IntNum)value.simplify()).getInt64();
+	}
+
+}
--- src/main/java/at/ac/tuwien/ec/sleipnir/Z3Solver.java
+++ src/main/java/at/ac/tuwien/ec/sleipnir/Z3Solver.java
@@ -1,24 +1,188 @@
 package at.ac.tuwien.ec.sleipnir;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+
 import com.microsoft.z3.*;
 
 public class Z3Solver {
 
 	public static void main(String[] arg)
 	{
-		Context ctx = new Context();
-		Solver solver = ctx.mkSimpleSolver();
-		final BoolExpr a = ctx.mkBoolConst("A");
-		final BoolExpr b = ctx.mkBoolConst("B");
-		final BoolExpr expr = ctx.mkOr(a,b);
+		final int num_task = 3;
+		final int num_processors = 3;
+		final double[] goals = new double[3];
 		
-		solver.add(expr);
-		solver.check();
-		final Model model = solver.getModel();
+		goals[0] = 8;
+		goals[1] = 8;
+		goals[2] = 8;
 		
-		System.out.println(model.getConstInterp(a));
-		System.out.println(model.getConstInterp(b));
+		System.err.println("java.library.path = " + System.getProperty("java.library.path"));
+		System.err.println("trying to load lib z3java");
+		try { 
+			System.load("/home/vincenzo/z3/build/libz3.so");
+			System.load("/home/vincenzo/z3/build/libz3java.so"); 
+		} catch (UnsatisfiedLinkError ex1) { 
+			ex1.printStackTrace();
+			try { 
+				System.err.println("Trying to load lib libz3java");
+				System.loadLibrary("libz3java"); 
+			} catch (UnsatisfiedLinkError ex2) { 
+				ex2.printStackTrace();
+			}
+		}
+
+		Map<String, String> config = new HashMap<String, String>();
+		config.put("model", "true");
+		Context ctx = new Context(config);
+		Solver solver = ctx.mkSolver();
+		
+		IntExpr[][] assVar = new IntExpr[num_task][num_processors];
+		
+		for(int i = 0; i < num_task; i++)
+			for(int j = 0; j < num_processors; j++)
+				assVar[i][j] = ctx.mkIntConst("x"+i+""+j);
+			
+		BoolExpr[] assignment = new BoolExpr[num_task];
+		
+		for(int i = 0; i < num_task; i++)
+		{
+			BoolExpr domain = ctx.mkBool(true);
+			ArithExpr sum = ctx.mkInt(0);
+			for(int j = 0; j < num_processors; j++) 
+			{
+				domain = ctx.mkAnd(domain,
+						ctx.mkOr(
+								ctx.mkEq(assVar[i][j].simplify(),ctx.mkInt(0)),
+								ctx.mkEq(assVar[i][j].simplify(),ctx.mkInt(1))
+								)
+						);
+				sum = ctx.mkAdd(sum,assVar[i][j]);
+			}
+			assignment[i] = ctx.mkAnd(domain,ctx.mkEq(sum, ctx.mkInt(1)));
+		}
+		RealExpr[][] runtimes = new RealExpr[num_task][num_processors];
+		RealExpr[][] costMatrix = new RealExpr[num_task][num_processors];
+		RealExpr[][] energyMatrix = new RealExpr[num_task][num_processors];
+		
+		Random rand = new Random();
+		System.out.println("RUNTIME");
+		for(int i = 0; i < num_task; i++)
+		{
+			for(int j = 0; j < num_processors; j++)
+			{
+				runtimes[i][j] = ctx.mkReal(""+(1.0 + (9.0) * rand.nextDouble()));
+				System.out.print(doubleValue(runtimes[i][j]) + " , ");
+			}
+			System.out.println("");
+		}
+		System.out.println("COST");
+		for(int i = 0; i < num_task; i++)
+		{
+			for(int j = 0; j < num_processors; j++)
+			{
+				costMatrix[i][j] = ctx.mkReal(""+(1.0 + (9.0) * rand.nextDouble()));
+				System.out.print(doubleValue(costMatrix[i][j]) + " , ");
+			}
+			System.out.println("");
+		}
+		System.out.println("ENERGY");
+		for(int i = 0; i < num_task; i++)
+		{
+			for(int j = 0; j < num_processors; j++)
+			{
+				energyMatrix[i][j] = ctx.mkReal(""+(1.0 + (9.0) * rand.nextDouble()));
+				System.out.print(doubleValue(energyMatrix[i][j])+ " , ");
+			}
+			System.out.println("");
+		}
+		
+		
+		ArithExpr[] runtimeForTask = new ArithExpr[num_task];
+		BoolExpr[] rtConstraints = new BoolExpr[num_task];
+		for(int i = 0; i < num_task; i++)
+		{
+			ArithExpr rt = ctx.mkMul(runtimes[i][0],assVar[i][0]);
+			runtimeForTask[i] = ctx.mkAdd(rt);
+			for(int j = 1; j < num_processors; j++)
+			{
+				rt = ctx.mkMul(runtimes[i][j],assVar[i][j]);
+				runtimeForTask[i] = ctx.mkAdd(runtimeForTask[i],rt);
+			}
+			rtConstraints[i] = ctx.mkLe(runtimeForTask[i], ctx.mkInt((int)goals[0]));
+			//System.out.println(rtConstraints[i]);
+		}
+		
+		ArithExpr[] costForTask = new ArithExpr[num_task];
+		BoolExpr[] costConstraints = new BoolExpr[num_task];
+		for(int i = 0; i < num_task; i++)
+		{
+			ArithExpr cost = ctx.mkMul(costMatrix[i][0],assVar[i][0]);
+			costForTask[i] = ctx.mkAdd(cost);
+			for(int j = 1; j < num_processors; j++)
+			{
+				cost = ctx.mkMul(costMatrix[i][j],assVar[i][j]);
+				costForTask[i] = ctx.mkAdd(runtimeForTask[i],cost);
+			}
+			costConstraints[i] = ctx.mkLe(costForTask[i], ctx.mkInt((int)goals[1]));
+			//System.out.println(costConstraints[i]);
+		}
+		
+		ArithExpr[] energyForTask = new ArithExpr[num_task];
+		BoolExpr[] energyConstraints = new BoolExpr[num_task];
+		for(int i = 0; i < num_task; i++)
+		{
+			ArithExpr energy = ctx.mkMul(energyMatrix[i][0],assVar[i][0]);
+			energyForTask[i] = ctx.mkAdd(energy);
+			for(int j = 1; j < num_processors; j++)
+			{
+				energy = ctx.mkMul(energyMatrix[i][j],assVar[i][j]);
+				energyForTask[i] = ctx.mkAdd(energyForTask[i],energy);
+			}
+			energyConstraints[i] = ctx.mkLe(energyForTask[i], ctx.mkInt((int)goals[2]));
+			//System.out.println(energyConstraints[i]);
+		}
+		
+		BoolExpr taskConstraints[] = new BoolExpr[num_task];
+		for(int i = 0; i < num_task; i++)
+			taskConstraints[i] = ctx.mkAnd(rtConstraints[i],costConstraints[i],energyConstraints[i]);
+		
+		solver.add(assignment);
+		solver.add(taskConstraints);
+				
+		Status status = solver.check();
+		
+		
+		Model model = null;
+		if(status == Status.SATISFIABLE)
+		{
+			model = solver.getModel();
+			System.out.println("Satisfiable!");
+			for(int i = 0; i < num_task; i++)
+				for(int j = 0; j < num_processors; j++)
+				{
+					if(model.getConstInterp(assVar[i][j]).equals(ctx.mkInt(1)))
+							System.out.println("Task "+i+ " assigned to " + j);
+					
+				}
+		}
+		//System.out.println(model);
+		
+		
 		
 		ctx.close();
+	}
+
+	private static double doubleValue(RealExpr realExpr) {
+		Expr value = realExpr.simplify();
+		if(value.isRatNum())
+		{
+			RatNum rational = (RatNum) value;
+			IntNum num = rational.getNumerator();
+			IntNum den = rational.getDenominator();
+			return ((double) num.getInt64() / den.getInt64());
+		}
+		return ((IntNum)value.simplify()).getInt64();
 	}
 }
